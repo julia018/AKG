69dc819f99a31439e5f7e695c0c8ab31cf3e1733
Yulia <kisliuk-y@mail.ru>
2020-11-06 10:02:21 +0300

    +view matrix

--- src/logic/Camera.java
+++ src/logic/Camera.java
@@ -1,19 +1,16 @@
 package logic;
 
+import model.Vector3;
+
 public class Camera {
 
     private Transformation position;
     private Transformation projection;
     private Transformation viewport;
+    private Transformation observer;
 
     private final double XMIN = 0.0;
     private final double YMIN = 0.0;
-
-    public Camera() {
-        this.position = new Transformation();
-        this.projection = new Transformation();
-        this.viewport = new Transformation();
-    }
 
     public void setOrthoProjection(double width, double height, double near, double far) {
         this.projection = new Transformation();
@@ -39,6 +36,26 @@ public class Camera {
         this.projection.setMatrixElement(3,  XMIN + width / 2.0);
         this.projection.setMatrixElement(5, -height / 2.0);
         this.projection.setMatrixElement(3,  YMIN + height / 2.0);
+    }
+
+    public void setObserver(Vector3 eye, Vector3 target, Vector3 up) {
+        this.observer = new Transformation();
+
+        Vector3 zAxis = eye.substractVector(target).getNormalized();
+        Vector3 xAxis = up.getCrossProduct(zAxis).getNormalized();
+        Vector3 yAxis = up;
+        this.projection.setMatrixElement(0, xAxis.getX());
+        this.projection.setMatrixElement(1,  xAxis.getY());
+        this.projection.setMatrixElement(2, xAxis.getZ());
+        this.projection.setMatrixElement(3,  -1 * xAxis.getScalarProduct(eye));
+        this.projection.setMatrixElement(4, yAxis.getX());
+        this.projection.setMatrixElement(5,  yAxis.getY());
+        this.projection.setMatrixElement(6, yAxis.getZ());
+        this.projection.setMatrixElement(7,  -1 * yAxis.getScalarProduct(eye));
+        this.projection.setMatrixElement(8, zAxis.getX());
+        this.projection.setMatrixElement(9,  zAxis.getY());
+        this.projection.setMatrixElement(10, zAxis.getZ());
+        this.projection.setMatrixElement(11,  -1 * zAxis.getScalarProduct(eye));
     }
 
 }
--- src/model/Vector3.java
+++ src/model/Vector3.java
@@ -12,4 +12,40 @@ public class Vector3 {
         this.y = y;
         this.z = z;
     }
+
+    public Vector3 getNormalized() {
+        float length = getLength();
+        float normX = this.x / length;
+        float normY = this.y / length;
+        float normZ = this.z / length;
+        return new Vector3(normX, normY, normZ);
+    }
+
+    private float getLength() {
+        return (float)Math.sqrt(this.x * this.x + this.y * this.y + this.z * this.z);
+    }
+
+    public Vector3 substractVector(Vector3 vector) {
+        return new Vector3(this.x - vector.x, this.y - vector.y, this.z - vector.z);
+    }
+
+    public Vector3 getCrossProduct(Vector3 vector) {
+        return new Vector3(this.y * vector.z - this.z * vector.y, this.z * vector.x - this.x * vector.z, this.x * vector.y - this.y * vector.x);
+    }
+
+    public float getScalarProduct(Vector3 vector) {
+        return (this.x * vector.x + this.y * vector.y + this.z * vector.z);
+    }
+
+    public float getX() {
+        return x;
+    }
+
+    public float getY() {
+        return y;
+    }
+
+    public float getZ() {
+        return z;
+    }
 }
